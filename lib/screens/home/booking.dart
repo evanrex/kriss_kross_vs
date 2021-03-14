d0abef07c3ef417b80994c541a8b7e3ff3c574e0
evanrex <69971103+evanrex@users.noreply.github.com>
2021-03-14 22:31:00 +0200

    Submit bookings to database with user data

--- lib/screens/home/booking.dart
+++ lib/screens/home/booking.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:kriss_kross_vs/models/location.dart';
 import 'package:kriss_kross_vs/models/user.dart';
 import 'package:kriss_kross_vs/services/database.dart';
+import 'package:kriss_kross_vs/shared/loading.dart';
 import 'package:provider/provider.dart';
 
 class Booking extends StatefulWidget {
@@ -49,84 +50,98 @@ class _BookingState extends State<Booking> {
 
   @override
   Widget build(BuildContext context) {
-    //final userData = Provider.of<UserData>(context);
-    return Scaffold(
-        backgroundColor: Colors.brown[100],
-        appBar: AppBar(
-          backgroundColor: Colors.brown[400],
-          elevation: 0.0,
-          title: Text('Book trip to ' + widget.location.name),
-        ),
-        body: Container(
-          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 20.0),
-                //drop down:
+    final user = Provider.of<User>(context);
+    return StreamBuilder<Object>(
+        stream: DatabaseService(uid: user.uid).userData,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            UserData userData = snapshot.data;
 
-                DropdownButtonFormField(
-                  hint: Text('Select desired ETA'),
-                  items: widget.location.arrivalTime.map((eta) {
-                    return DropdownMenuItem(
-                        value: eta, child: Text('ETA: ' + '$eta'));
-                  }).toList(),
-                  onChanged: (value) => setState(() => _desiredETA = value),
+            return Scaffold(
+                backgroundColor: Colors.brown[100],
+                appBar: AppBar(
+                  backgroundColor: Colors.brown[400],
+                  elevation: 0.0,
+                  title: Text('Book trip to ' + widget.location.name),
                 ),
+                body: Container(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 20.0),
+                        //drop down:
 
-                SizedBox(height: 20.0),
-                DropdownButtonFormField(
-                  hint: Text('Select desired boarding point'),
-                  items: widget.location.pickUpLocations.map((point) {
-                    return DropdownMenuItem(
-                        value: point, child: Text('Board at: ' + '$point'));
-                  }).toList(),
-                  onChanged: (value) => setState(() => _desiredPickUp = value),
-                ),
-
-                SizedBox(height: 20.0),
-                SizedBox(height: 20.0),
-
-                Text(pickupTime(
-                    widget.location.travelTimes, _desiredPickUp, _desiredETA)),
-                SizedBox(height: 20.0),
-                Row(
-                  children: [
-                    ElevatedButton(
-                        //color: Colors.pink[400],
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.pink[400],
+                        DropdownButtonFormField(
+                          hint: Text('Select desired ETA'),
+                          items: widget.location.arrivalTime.map((eta) {
+                            return DropdownMenuItem(
+                                value: eta, child: Text('ETA: ' + '$eta'));
+                          }).toList(),
+                          onChanged: (value) =>
+                              setState(() => _desiredETA = value),
                         ),
-                        child:
-                            Text('Book', style: TextStyle(color: Colors.white)),
-                        onPressed: () async {
-                          await DatabaseService().makeBooking(
-                            // userData.name,
-                            // userData.phone,
-                            'name',
-                            'phone',
-                            widget.location.name,
-                            _desiredETA,
-                            _desiredPickUp,
-                          );
-                          Navigator.pop(context);
-                        }),
-                    SizedBox(width: 20.0),
-                    ElevatedButton(
-                        //color: Colors.pink[400],
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.pink[400],
+
+                        SizedBox(height: 20.0),
+                        DropdownButtonFormField(
+                          hint: Text('Select desired boarding point'),
+                          items: widget.location.pickUpLocations.map((point) {
+                            return DropdownMenuItem(
+                                value: point,
+                                child: Text('Board at: ' + '$point'));
+                          }).toList(),
+                          onChanged: (value) =>
+                              setState(() => _desiredPickUp = value),
                         ),
-                        child: Text('Cancel',
-                            style: TextStyle(color: Colors.white)),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        }),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ));
+
+                        SizedBox(height: 20.0),
+                        SizedBox(height: 20.0),
+
+                        Text(pickupTime(widget.location.travelTimes,
+                            _desiredPickUp, _desiredETA)),
+                        SizedBox(height: 20.0),
+                        Row(
+                          children: [
+                            ElevatedButton(
+                                //color: Colors.pink[400],
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.pink[400],
+                                ),
+                                child: Text('Book',
+                                    style: TextStyle(color: Colors.white)),
+                                onPressed: () async {
+                                  await DatabaseService().makeBooking(
+                                    userData.name,
+                                    userData.phone,
+                                    // 'name',
+                                    // 'phone',
+                                    widget.location.name,
+                                    _desiredETA,
+                                    _desiredPickUp,
+                                  );
+                                  Navigator.pop(context);
+                                }),
+                            SizedBox(width: 20.0),
+                            ElevatedButton(
+                                //color: Colors.pink[400],
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.pink[400],
+                                ),
+                                child: Text('Cancel',
+                                    style: TextStyle(color: Colors.white)),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                }),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ));
+          } else {
+            return Loading();
+          }
+        });
   }
 }
